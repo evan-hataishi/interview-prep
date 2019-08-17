dd9f545edf89fde6d05dde9e6ad088338d635fa4
Evan Hataishi <evanhata@hawaii.edu>
2019-08-16 22:24:56 -1000

    more fib!

--- python_fun/memoize.py
+++ python_fun/memoize.py
@@ -20,6 +20,20 @@ def fib(n):
 		return fib(n - 1) + fib(n - 2)
 
 
+def iter_fib(n):
+	if n == 0:
+		return 0
+	prev, curr = 0, 1
+	for i in range(2, n + 1):
+		prev, curr = curr, prev + curr
+	return curr
+
+
+# TODO hmm
+def recursive_bott_up_fib(n):
+	pass
+	
+
 if __name__ == "__main__":
 	num = int(argv[1])
 	print(fib(num))
